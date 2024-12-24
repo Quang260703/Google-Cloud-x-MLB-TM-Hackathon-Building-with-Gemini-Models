8e59a5f95b8c9d54749004d7328942b3562fdfb0
Quang Nguyen <nguye955@msu.edu>
2024-12-24 16:37:28 -0500

    Getting data from statsapi

---
+++ mlb.py
@@ -0,0 +1,20 @@
+import pprint
+from pprint import pprint
+import requests
+
+# Example: Get Dodgers roster
+#url = "https://statsapi.mlb.com/api/v1.1/game/634675/feed/live"
+#url = "https://statsapi.mlb.com/api/v1.1/game/634675/feed/live/timestamps"
+url = "https://statsapi.mlb.com/api/v1.1/game/634675/feed/live?timecode=20210714_031900"
+response = requests.get(url)
+data = response.json()
+
+# Extract all plays
+plays = data.get("liveData", {}).get("plays", {}).get("allPlays", [])
+
+# Extract descriptions
+descriptions = [play.get("result", {}).get("description", "No description available") for play in plays]
+
+# Print the list of descriptions
+for description in descriptions:
+    print(description)
