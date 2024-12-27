9a81a7c632739f89fb5729487d306387613f0fae
Quang Nguyen <nguye955@msu.edu>
2024-12-26 20:10:37 -0500

    Import primeng calendar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CalendarModule } from "primeng/calendar"; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, CalendarModule, FormsModule], // Import necessary modules
+  templateUrl: './app.component.html', // The template for the component
 })
 export class AppComponent {
   title = 'frontend';
+  "date": Date;
 }
