4187c51de877b5f2317216700009ef4dba91cce0
Quang Nguyen <nguye955@msu.edu>
2025-01-02 21:26:15 -0500

    Display match results and time

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { DatePickerModule } from 'primeng/datepicker';
 import { AppService } from './app.service';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Subscription, firstValueFrom } from 'rxjs';
+import { Team } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ import { Subscription, firstValueFrom } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'frontend';
-  teamNames: { awayTeam: string, homeTeam: string }[] = [];
+  teamNames: { awayTeam: Team, homeTeam: Team, detailedState: string, time: string }[] = [];
   selectedDate: Date = new Date(2024, 2, 18);
   interval: Subscription | undefined;
   isBrowser = signal(false);
@@ -54,17 +55,36 @@ export class AppComponent implements OnInit, OnDestroy {
     if (date != null) {
       try {
         const response = await firstValueFrom(this.appService.getDataByDate(date));
-        const games = response.dates[0].games;
-        this.teamNames = games.map((play: any) => {
-          return {
-            awayTeam: play.teams.away.team.name,
-            homeTeam: play.teams.home.team.name,
-          };
-        });
+        const games = response.dates[0]?.games || [];
+        if (games.length > 0) {
+          this.teamNames = games.map((play: any) => {
+            return {
+              awayTeam: play.teams.away,
+              homeTeam: play.teams.home,
+              detailedState: play.status.detailedState,
+              time: this.getLocalTime(new Date(play.gameDate))
+            };
+          });
+        }
+        else {
+          this.teamNames = [];
+        }
       } catch (error) {
-        this.teamNames = []
+        console.log(error);
       }
     }
+  }
+
+  getLocalTime(date: Date) {
+    // Format the time in the user's local timezone in 24-hour format (hour and minute only)
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+      timeZone: 'America/New_York',
+      timeZoneName: 'short',
+    });
+    return formatter.format(date)
   }
 
   getLogoUrl(teamName: string): string {
---
+++ frontend/src/app/interface.ts
@@ -0,0 +1,6 @@
+export interface Team {
+    team: {
+        name: string
+    },
+    score?: string
+}
